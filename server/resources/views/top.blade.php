0c58f4f206f4a8b70f0183923ab1ea8bf3ea1fa5
AiRKING <ryuuhei1110512@gmail.com>
2019-12-08 23:21:48 +0900

    [modify] nginx + laravel環境設定

---
+++ server/resources/views/top.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+               <h1>トップページ</h1>
+            </div>
+        </div>
+    </body>
+</html>
